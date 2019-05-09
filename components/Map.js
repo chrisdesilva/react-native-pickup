ccdcfb85c3a126ee2fbe3acd7c1f4f47cde3a60d
cdesilva <chris.a.desilva@gmail.com>
2019-05-09 15:53:46 -0500

    Add Map component

---
+++ components/Map.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { MapView } from 'expo'
+
+const Marker = MapView.Marker
+
+class Map extends React.Component {
+    renderMarkers() {
+        return this.props.places.map((place, i) => (
+            <Marker key={i} title={place.name} coordinate={place.coords} />
+        ))
+    }
+
+    render() {
+        const { region } = this.props
+        return (
+            <MapView 
+                style={styles.container}
+                region={region}
+                showsUserLocation
+                showsMyLocationButton
+            >
+            {this.renderMarkers()}
+            </MapView>
+        )
+    }
+}
+
+const styles = {
+    container: {
+        width: '100%',
+        height: '80%'
+    }
+}
+
+export default Map
